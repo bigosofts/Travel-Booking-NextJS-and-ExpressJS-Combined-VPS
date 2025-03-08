2c3518efe1b636799eff44d7321a6cdbdfa110c9
bigosofts <abdullah.limonbau@gmail.com>
2025-03-08 09:32:20 +0600

    Chnage payment processing method 2

--- src/controllers/paymentProcessing.js
+++ src/controllers/paymentProcessing.js
@@ -7,30 +7,26 @@ exports.paymentProcessing = (req, res) => {
   //Receive Post Request Data from req body
   let reqBody = req.body;
 
-  // Replace these with your actual Client ID and Client Secret
   const CLIENT_ID = process.env.CLIENT_ID;
   const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-  // Encode credentials to Base64
-  const encodedCredentials = Buffer.from(
-    `${CLIENT_ID}:${CLIENT_SECRET}`
-  ).toString("base64");
 
   async function getAccessToken() {
     try {
       const response = await axios.post(
-        "https://demo-accounts.vivapayments.com/connect/token",
+        "https://accounts.vivapayments.com/connect/token",
         qs.stringify({ grant_type: "client_credentials" }),
         {
+          auth: {
+            username: CLIENT_ID,
+            password: CLIENT_SECRET,
+          },
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${encodedCredentials}`,
           },
         }
       );
 
       console.log("Access Token:", response.data.access_token);
-      res.status(200).json({ "Access Token:": response.data.access_token });
       return response.data.access_token;
     } catch (error) {
       console.error(
@@ -40,6 +36,5 @@ exports.paymentProcessing = (req, res) => {
     }
   }
 
-  // Call function to get token
   getAccessToken();
 };
